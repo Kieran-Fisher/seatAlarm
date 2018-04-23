b6f5da63e3e93ada1b5ee3cebf5a6b5d0e7fccad
Kieran-Fisher <36045604+Kieran-Fisher@users.noreply.github.com>
2018-04-23 10:30:02 +0100

    Add files via upload

---
+++ Arduino Code/Sensor_activation/Sensor_activation.ino
@@ -0,0 +1,33 @@
+#include <math.h>
+
+int sensorPin = A0;    // select the input pin for the potentiometer
+int ledPin = 13;      // select the pin for the LED
+int sensorValue = false;  // variable to store the value coming from the sensor
+
+
+void setup(){
+  Serial.begin(9600);//initialize the serial monitor at 9600 baud rate
+      // declare the ledPin as an OUTPUT:
+  pinMode(ledPin, OUTPUT);
+  if(sensorValue >= 100){
+    sensorValue = true;
+    }
+}
+
+void loop(){
+  int sensorValue=analogRead(sensorPin);//create a var to store the value of the sensor
+  Serial.println("the analog read data is ");//print on the serial monitor what's in the ""
+  Serial.println(sensorValue);// print the value of the sensor on the serial monitor
+  delay(1000);
+    // read the value from the sensor:
+  sensorValue = analogRead(sensorPin);
+  // turn the ledPin on
+  digitalWrite(ledPin, HIGH);
+  // stop the program for <sensorValue> milliseconds:
+  delay(sensorValue);
+  // turn the ledPin off:
+  digitalWrite(ledPin, LOW);
+  // stop the program for for <sensorValue> milliseconds:
+  delay(sensorValue);
+  
+}
---
+++ Arduino Code/light_sensor/light_sensor.ino
@@ -0,0 +1,22 @@
+int sensorPin = A0;    // select the input pin for the potentiometer
+int LED = 2; //the pin we connect the LED
+int sensorValue = 0;  // variable to store the value coming from the sensor
+
+void setup() {
+  // declare the ledPin as an OUTPUT:
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(9600); //sets serial port for communication
+}
+
+void loop() {
+  // read the value from the sensor:
+  sensorValue = analogRead(sensorPin);
+  // turn the ledPin on
+  Serial.println(sensorValue); //prints results of sensor on the screen
+  digitalWrite(ledPin, HIGH);
+  // stop the program for <sensorValue> milliseconds:
+  delay(sensorValue);
+  digitalWrite(ledPin, LOW);
+  // stop the program for for <sensorValue> milliseconds:
+  delay(sensorValue);
+}
